41bad01362c96afe73dbd9c6d1c618e3419f9d01
shreyaag25 <100709661+shreyaag25@users.noreply.github.com>
2023-09-29 09:53:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (40.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,38 +1,69 @@
 class Solution {
 public:
 
-    bool isSafe(int col, int row, vector<string> &board, int n)
-    {
-        int dupRow = row; 
-        int dupCol = col;
-        while(row>=0 && col>=0)
-        {
-            if(board[row][col]=='Q')
-                return false;
-            row--;
-            col--;
-        }
-        row = dupRow;
-        col = dupCol;
-        while(col>=0)
-        {
-            if(board[row][col]=='Q')
-                return false;
-            col--;
-        }
-        col = dupCol;
-        row = dupRow;
-        while(col>=0 && row<n)
-        {
-            if(board[row][col]=='Q')
-                return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
+    // bool isSafe(int col, int row, vector<string> &board, int n)
+    // {
+    //     int dupRow = row; 
+    //     int dupCol = col;
+    //     //check upper diagonal
+    //     while(row>=0 && col>=0)
+    //     {
+    //         if(board[row][col]=='Q')
+    //             return false;
+    //         row--;
+    //         col--;
+    //     }
+    //     row = dupRow;
+    //     col = dupCol;
+    //     //check left row
+    //     while(col>=0)
+    //     {
+    //         if(board[row][col]=='Q')
+    //             return false;
+    //         col--;
+    //     }
+    //     col = dupCol;
+    //     row = dupRow;
+    //     //check lower diagonal
+    //     while(col>=0 && row<n)
+    //     {
+    //         if(board[row][col]=='Q')
+    //             return false;
+    //         col--;
+    //         row++;
+    //     }
+    //     return true;
+    // }
 
-    void solve(int col, int n ,vector<string> &board, vector<vector<string>> &ans)
+    // void solve(int col, int n ,vector<string> &board, vector<vector<string>> &ans)
+    // {
+    //     if(col==n)
+    //     {
+    //         ans.push_back(board);
+    //         return;
+    //     }
+    //     for(int row =0;row<n;row++)
+    //     {
+    //         if(isSafe(col, row, board , n))
+    //         {
+    //             board[row][col]='Q';
+    //             solve(col+1, n, board, ans);
+    //             board[row][col]='.';  //backtrack(going back)
+    //         }
+    //     }
+    // }
+    // vector<vector<string>> solveNQueens(int n) {
+    //     vector<vector<string>> ans;
+    //     vector<string> board (n);
+    //     string s(n, '.');
+    //     for(int i =0;i<n;i++)
+    //         board[i]= s;
+    //     solve(0, n, board, ans);
+    //     return ans;
+    // }
+
+
+    void solve(int col, int n ,vector<string> &board, vector<vector<string>> &ans, vector<int> &leftRow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal)
     {
         if(col==n)
         {
@@ -41,11 +72,17 @@ public:
         }
         for(int row =0;row<n;row++)
         {
-            if(isSafe(col, row, board , n))
+            if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0)
             {
                 board[row][col]='Q';
-                solve(col+1, n, board, ans);
-                board[row][col]='.';
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1+col-row]= 1;
+                solve(col+1, n, board, ans,leftRow, upperDiagonal, lowerDiagonal);
+                board[row][col]='.';  //backtrack(going back)
+                leftRow[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1+col-row]= 0;
             }
         }
     }
@@ -55,7 +92,8 @@ public:
         string s(n, '.');
         for(int i =0;i<n;i++)
             board[i]= s;
-        solve(0, n, board, ans);
+        vector<int> leftRow(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
+        solve(0, n, board, ans,leftRow, upperDiagonal, lowerDiagonal);
         return ans;
     }
 };
