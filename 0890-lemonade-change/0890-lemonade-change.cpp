9b5fb696974f5a8d183048d85bf5446a72b1d764
shreyaag25 <100709661+shreyaag25@users.noreply.github.com>
2023-09-30 20:35:46 +0530

    Time: 88 ms (16.98%), Space: 83.8 MB (5.34%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten=0;
+        for(auto it : bills)
+        {
+            if(it==5)
+                five++;
+            else if(it == 10)
+            {
+                if(five>0)
+                {
+                    ten++;
+                    five--;
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                if(five>0 && ten>0)
+                {
+                    five--;
+                    ten--;
+                }
+                else if(five>=3)
+                    five-=3;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+};
