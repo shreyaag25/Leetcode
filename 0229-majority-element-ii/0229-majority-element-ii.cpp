e94449486a37e61bb4c7a3f84a5f1050801e6051
shreyaag25 <100709661+shreyaag25@users.noreply.github.com>
2023-10-05 18:54:32 +0530

    Time: 12 ms (30.64%), Space: 16.4 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
         map<int,int> mp;
+        int minValue = nums.size()/3 +1;
         for(auto num : nums)
-            mp[num]++;
-        for(auto it : mp)
         {
-            if(it.second>nums.size()/3)
-                ans.push_back(it.first);
+            mp[num]++;
+            if(mp[num] == minValue)
+                ans.push_back(num);
         }
         return ans;
     }
