be227418082f78f0092984426b480931c6a029b6
Shreya Agrawal <100709661+shreyaag25@users.noreply.github.com>
2023-10-16 01:02:19 +0530

    Time: 6 ms (10.4%), Space: 8.1 MB (19.93%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -19,7 +19,36 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return find(m-1, n-1, dp, obstacleGrid);
+        // vector<vector<int>> dp(m, vector<int>(n,-1));
+        //memoization
+        // return find(m-1, n-1, dp, obstacleGrid);
+
+    //tabulation
+    vector<vector<int>> dp(m, vector<int>(n,0));
+        for(int i =0; i<m; i++)
+        {
+            for(int j = 0; j<n ; j++)
+            {
+                if(i!=0 || j!=0)
+                {
+                    if(obstacleGrid[i][j]==0)
+                    {
+                        int up = 0, left =0;
+                        if(i>0)
+                            up = dp[i-1][j];
+                        if(j>0)
+                            left = dp[i][j-1];
+                        dp[i][j] = up+left;
+                    }
+                    else
+                        dp[i][j] = 0;
+                }
+                else if(obstacleGrid[i][j] ==0)
+                { 
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
