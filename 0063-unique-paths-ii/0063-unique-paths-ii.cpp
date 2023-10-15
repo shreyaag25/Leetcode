c9d7a61349ba0897ebde98ff0abd72e56af4afbd
Shreya Agrawal <100709661+shreyaag25@users.noreply.github.com>
2023-10-16 01:05:15 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.28%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -29,23 +29,21 @@ public:
         {
             for(int j = 0; j<n ; j++)
             {
-                if(i!=0 || j!=0)
+                if(obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+
+                else if(i==0 && j==0)
                 {
-                    if(obstacleGrid[i][j]==0)
-                    {
-                        int up = 0, left =0;
-                        if(i>0)
-                            up = dp[i-1][j];
-                        if(j>0)
-                            left = dp[i][j-1];
-                        dp[i][j] = up+left;
-                    }
-                    else
-                        dp[i][j] = 0;
+                    dp[i][j]=1;
                 }
-                else if(obstacleGrid[i][j] ==0)
-                { 
-                    dp[i][j] = 1;
+                else
+                {
+                    int up = 0, left =0;
+                    if(i>0)
+                            up = dp[i-1][j];
+                    if(j>0)
+                            left = dp[i][j-1];
+                    dp[i][j] = up+left;
                 }
             }
         }
