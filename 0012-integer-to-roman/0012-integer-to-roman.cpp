241763a6717d1ed0197791ea6a512aae8b34faac
Shreya Agrawal <100709661+shreyaag25@users.noreply.github.com>
2023-11-09 18:20:30 +0530

    Time: 8 ms (46.46%), Space: 10 MB (13%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        //tc - O(1)
+        //space - O(1)
         vector<pair<int, string>> mapping = {{1000, "M"},{900, "CM"},{500, "D"},{400, "CD"},{100, "C"},{90, "XC"},{50, "L"},{40, "XL"},{10, "X"},{9, "IX"},{5, "V"},{4, "IV"},{1, "I"}};
         string roman = "";
         for(auto np : mapping)
